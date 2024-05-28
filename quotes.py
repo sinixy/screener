c1bf864c66a8323e1effc0ee57b75a114666dcce
HLIB SIN <sinixyowo@gmail.com>
2024-05-28 12:20:48 +0300

    t5 unpacking

--- quotes.py
+++ quotes.py
@@ -1,5 +1,6 @@
 from scapy.all import sniff, Raw
 from scapy.layers.l2 import Ether
+from datetime import datetime
 
 from struct import unpack
 
@@ -10,9 +11,21 @@ def pprint(packet: Ether):
     
     data = packet[Raw].load
     message_length, message_type = unpack('<HH', data[:4])
-    if message_type == 14:
+    if message_type == 5:
+        print(f'len={message_length} | type={message_type}')
+        time = datetime.fromtimestamp(
+            datetime.today().replace(hour=0, minute=0, second=0, microsecond=0).timestamp() + unpack('<I', data[4:8])[0] / 1000
+        )
+        symbol = str(data[8:16]).replace('\\x00', '')
+        shares = unpack('<I', data[32:36])[0]
+        print(f'time={time} | symbol={symbol} | shares={shares}')
+
+        # i = 16
+        # while i <= message_length - 4:
+        #     print(f'{unpack("<I", data[i:i+4])[0]} -> {i}:{i+4}', end=' | ')
+        #     i += 1
+
         print(data)
-    print(message_length, message_type)
     
 
 capture = sniff(filter='ip src 185.41.250.207', prn=pprint)
